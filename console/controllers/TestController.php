cfc5a791788aa1b650fc6d2a2c611689a69c69ad
liujiankang <liujiankang1@foxmail.com>
2017-01-17 15:58:42 +0800

    vender

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -7,6 +7,7 @@ namespace console\controllers;
  * Date: 2016/11/16
  * Time: 21:51
  */
+use common\servers\tencent\GpEverydayDealDetails;
 use Faker\Provider\UserAgent;
 use common\lib\http\WgetTransfer;
 use common\servers\sina\GpEverydayDealDetails as sinaED;
@@ -18,21 +19,7 @@ class TestController extends \yii\console\Controller
 {
     public function actionIndex(){
         echo UserAgent::userAgent();
-//        $key=md5(__METHOD__);
-//        $isHave=Yii::$app->cache->get($key);
-//        if(!$isHave || $isHave<time()-5){
-//            Yii::$app->cache->set($key,time(),10);
-//            for($i=0;$i<20;$i++){
-//                echo 'sleep 10s'.Yii::$app->cache->get($key).PHP_EOL;
-//                sleep(1);
-//                if($i<7){
-//                    Yii::$app->cache->set($key,time(),10);
-//                }
-//            }
-//            echo ' for loop done '.Yii::$app->cache->get($key).PHP_EOL;
-//        }else{
-//            echo ' already have'.PHP_EOL;
-//        }
+        (new GpEverydayDealDetails())->actionOneSyn();
     }
     public function actionRunsina()
     {
